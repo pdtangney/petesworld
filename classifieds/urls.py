635cbc765ea3dbc283c02b70a8e1eb9951053881
Peter Tangney <peter@peterswebworld.net>
2023-10-20 23:50:37 -0400

    classifieds - add templates
    Add templates for classifieds. Users can now browse classifieds, sorted
    by category.

--- classifieds/urls.py
+++ classifieds/urls.py
@@ -7,4 +7,6 @@ app_name = 'classifieds'
 
 urlpatterns = [
         path('', views.index, name='index'),
+        path('categories/', views.categories, name='categories'),
+        path('categories/<int:category_id>/', views.category,name='category'),
         ]
--- classifieds/views.py
+++ classifieds/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 
+from .models import Category, ClassifiedAd
+
 def index(request):
     """The homepage for classifieds."""
     return render(request, 'classifieds/index.html')
+
+def categories(request):
+    """Show all categories."""
+    categories = Category.objects.order_by('category')
+    #{key:val} where key is the name used in the template, val is the data
+    # we're sending to the template!
+    context = {'categories':categories}
+    return render(request, 'classifieds/categories.html', context)
+
+def category(request, category_id):
+    """Show a single category and all its classified ads."""
+    category = Category.objects.get(id=category_id)
+    entries = category.classifiedad_set.order_by('-date_added')
+    context = {'category':category, 'entries':entries}
+    return render(request, 'classifieds/category.html', context)
