14abe23d2a14c3dd24356fff734be6b645d01bcb
Peter Tangney <peter@peterswebworld.net>
2023-10-21 21:06:45 -0400

    Add proper HTML to templates
    Add basic HTML tags, for proper compliance with standards. At this
    point, just the bare needed tags are used. Also, add page title
    variable, set via a render response.

--- classifieds/views.py
+++ classifieds/views.py
@@ -4,19 +4,21 @@ from .models import Category, ClassifiedAd
 
 def index(request):
     """The homepage for classifieds."""
-    return render(request, 'classifieds/index.html')
+    context = { 'title':'Classifieds - Home'}
+    return render(request, 'classifieds/index.html', context)
 
 def categories(request):
     """Show all categories."""
     categories = Category.objects.order_by('category')
     #{key:val} where key is the name used in the template, val is the data
     # we're sending to the template!
-    context = {'categories':categories}
+    context = {'categories':categories, 'title':'Classifieds - Categories'}
     return render(request, 'classifieds/categories.html', context)
 
 def category(request, category_id):
     """Show a single category and all its classified ads."""
     category = Category.objects.get(id=category_id)
     entries = category.classifiedad_set.order_by('-date_added')
-    context = {'category':category, 'entries':entries}
+    context = {'category':category, 'entries':entries,
+               'title':f'Classifieds - {category}'}
     return render(request, 'classifieds/category.html', context)
